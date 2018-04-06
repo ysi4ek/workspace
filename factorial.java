caca8869598e9cd2ec231dd366ce671958be889d
ysi4ek <37455356+ysi4ek@users.noreply.github.com>
2018-04-06 19:06:16 +0300

    dsad

---
+++ factorial.java
@@ -0,0 +1,10 @@
+package factorial;
+
+public class factorial {
+	public static void main(String[] args){
+	      int a=10, b=15;
+	        for (int i=1; i<=b; ++i) 
+	        	a *= i;
+System.out.println("факториал " + a);
+}
+}
